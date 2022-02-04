7f466724ec36d6730af808a79d15ddecfdff5c7b
Shah <abhinav.shah@nokia.com>
2022-02-03 21:38:20 -0500

    first commit

---
+++ projectmanagementtool/src/test/java/com/spring/personal/projectmanagementtool/ProjectmanagementtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.personal.projectmanagementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagementtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
